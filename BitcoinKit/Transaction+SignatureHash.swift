766c876697b016077acffc5cf328f477a2c772ba
Shun Usami <usatie@gmail.com>
2018-07-06 14:40:40 +0200

    :recycle: Refactor SighashType

--- BitcoinKit/Transaction+SignatureHash.swift
+++ BitcoinKit/Transaction+SignatureHash.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 extension Transaction {
-    public func signatureHash(for utxoToSign: TransactionOutput, inputIndex: Int, hashType: UInt8) -> Data {
+    public func signatureHash(for utxoToSign: TransactionOutput, inputIndex: Int, hashType: SighashType) -> Data {
         // Can't have index greater than num of inputs
         guard inputIndex < inputs.count else {
             return Data()
@@ -47,8 +47,8 @@ extension Transaction {
 }
 
 private extension Array where Element == TransactionInput {
-    func hashPrevouts(_ hashType: UInt8) -> Data {
-        if (hashType & SighashType.SIGHASH_ANYONECANPAY) == 0 {
+    func hashPrevouts(_ hashType: SighashType) -> Data {
+        if !hashType.isAnyoneCanPay {
             // If the ANYONECANPAY flag is not set, hashPrevouts is the double SHA256 of the serialization of all input outpoints
             let serializedPrevouts: Data = reduce(Data()) { $0 + $1.previousOutput.serialized() }
             return Crypto.sha256sha256(serializedPrevouts)
@@ -58,10 +58,10 @@ private extension Array where Element == TransactionInput {
         }
     }
 
-    func hashSequence(_ hashType: UInt8) -> Data {
-        if (hashType & SighashType.SIGHASH_ANYONECANPAY) == 0
-            && (hashType & 0x1f) != SighashType.SIGHASH_SINGLE
-            && (hashType & 0x1f) != SighashType.SIGHASH_NONE {
+    func hashSequence(_ hashType: SighashType) -> Data {
+        if !hashType.isAnyoneCanPay
+            && !hashType.isSingle
+            && !hashType.isNone {
             // If none of the ANYONECANPAY, SINGLE, NONE sighash type is set, hashSequence is the double SHA256 of the serialization of nSequence of all inputs
             let serializedSequence: Data = reduce(Data()) { $0 + $1.sequence }
             return Crypto.sha256sha256(serializedSequence)
@@ -73,14 +73,14 @@ private extension Array where Element == TransactionInput {
 }
 
 private extension Array where Element == TransactionOutput {
-    func hashOutputs(_ hashType: UInt8, inputIndex: Int) -> Data {
-        if (hashType & 0x1f) != SighashType.SIGHASH_SINGLE
-            && (hashType & 0x1f) != SighashType.SIGHASH_NONE {
+    func hashOutputs(_ hashType: SighashType, inputIndex: Int) -> Data {
+        if !hashType.isSingle
+            && !hashType.isNone {
             // If the sighash type is neither SINGLE nor NONE, hashOutputs is the double SHA256 of the serialization of all output amounts (8-byte little endian) paired up with their scriptPubKey (serialized as scripts inside CTxOuts)
             let serializedOutputs: Data = reduce(Data()) { $0 + $1.serialized() }
             return Crypto.sha256sha256(serializedOutputs)
 
-        } else if (hashType & 0x1f) == SighashType.SIGHASH_SINGLE && inputIndex < count {
+        } else if hashType.isSingle && inputIndex < count {
             // If sighash type is SINGLE and the input index is smaller than the number of outputs, hashOutputs is the double SHA256 of the output amount with scriptPubKey of the same index as the input
             let serializedOutput = self[inputIndex].serialized()
             return Crypto.sha256sha256(serializedOutput)
