80eb39d3d10bd5804ea53448608fb3713a92eb29
Shun Usami <usatie@gmail.com>
2018-07-08 08:57:46 +0200

    :sparkles: Add pubkey compression information in PrivateKey

--- BitcoinKit/PublicKey.swift
+++ BitcoinKit/PublicKey.swift
@@ -11,16 +11,23 @@ import BitcoinKit.Private
 
 public struct PublicKey {
     let raw: Data
+    var pubkeyHash: Data {
+        return Crypto.sha256ripemd160(raw)
+    }
     public let network: Network
+    public let isCompressed: Bool
 
-    init(privateKey: PrivateKey, network: Network) {
-        self.network = network
-        self.raw = PublicKey.from(privateKey: privateKey.raw)
+    init(privateKey: PrivateKey) {
+        self.network = privateKey.network
+        self.isCompressed = privateKey.isPublicKeyCompressed
+        self.raw = PublicKey.from(privateKey: privateKey.raw, compression: privateKey.isPublicKeyCompressed)
     }
 
     init(bytes raw: Data, network: Network) {
         self.raw = raw
         self.network = network
+        let header = raw[0]
+        self.isCompressed = (header == 0x02 || header == 0x03)
     }
 
     /// Version = 1 byte of 0 (zero); on the test network, this is 1 byte of 111
@@ -28,13 +35,13 @@ public struct PublicKey {
     /// Checksum = 1st 4 bytes of SHA-256(SHA-256(Key hash))
     /// Bitcoin Address = Base58Encode(Key hash concatenated with Checksum)
     public func toAddress() -> String {
-        let hash = Data([network.pubkeyhash]) + Crypto.sha256ripemd160(raw)
-        return publicKeyHashToAddress(hash)
+        let versionByte: Data = Data([network.pubkeyhash])
+        return publicKeyHashToAddress(versionByte + pubkeyHash)
     }
 
     public func toCashaddr() -> String {
-        let hash = Data([VersionByte.pubkeyHash160]) + Crypto.sha256ripemd160(raw)
-        return Bech32.encode(hash, prefix: network.scheme)
+        let versionByte: Data = Data([VersionByte.pubkeyHash160])
+        return Bech32.encode(versionByte + pubkeyHash, prefix: network.scheme)
     }
 
     static func from(privateKey raw: Data, compression: Bool = false) -> Data {
