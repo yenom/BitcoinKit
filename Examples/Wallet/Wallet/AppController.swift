761096a0c08053999940867a13a4f352e6c3b102
Shun Usami <usatie@gmail.com>
2018-07-11 10:20:36 +0200

    :recycle: Modify Wallet example so that it can send transaction

--- Examples/Wallet/Wallet/AppController.swift
+++ Examples/Wallet/Wallet/AppController.swift
@@ -59,3 +59,25 @@ extension Notification.Name {
         
     }
 }
+
+extension Data {
+    init?(hex: String) {
+        let len = hex.count / 2
+        var data = Data(capacity: len)
+        for i in 0..<len {
+            let j = hex.index(hex.startIndex, offsetBy: i * 2)
+            let k = hex.index(j, offsetBy: 2)
+            let bytes = hex[j..<k]
+            if var num = UInt8(bytes, radix: 16) {
+                data.append(&num, count: 1)
+            } else {
+                return nil
+            }
+        }
+        self = data
+    }
+    
+    var hex: String {
+        return reduce("") { $0 + String(format: "%02x", $1) }
+    }
+}
