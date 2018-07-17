76308367d7c21701a3f4e2f12aacfde5bef8a9b2
Shun Usami <usatie@gmail.com>
2018-07-17 15:36:22 +0900

    :sparkles: Add copyright information

--- BitcoinKit/TransactionSignatureSerializer.swift
+++ BitcoinKit/TransactionSignatureSerializer.swift
@@ -3,7 +3,22 @@
 //  BitcoinKit
 //
 //  Created by Shun Usami on 2018/07/06.
-//  Copyright © 2018 Kishikawa Katsumi. All rights reserved.
+//  Copyright © 2018 Shun Usami
+//  Copyright © 2018 BitcoinKit-cash developers
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//  This file has been modified by the BitcoinKit-cash developers for the BitcoinKit-cash project.
+//  The original file was from the bitcoinj project (https://github.com/kishikawakatsumi/BitcoinKit).
 //
 
 import Foundation
