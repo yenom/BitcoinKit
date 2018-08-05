e5223ffe7c3b85341f08e93cd01272b3329e82af
藤田哲史 <tessitetsusi@gmail.com>
2018-08-05 12:26:54 +0900

    :pencil2: add OP_NOT

--- BitcoinCashKit/Core/Scripts/Opcode.swift
+++ BitcoinCashKit/Core/Scripts/Opcode.swift
@@ -125,7 +125,7 @@ public struct OpCode {
     public static let OP_2DIV: OpCodeProtocol = OpExample() // Disabled opcode. If executed, transaction is invalid.
     public static let OP_NEGATE: OpCodeProtocol = OpExample() // negates the number, pops it from stack and pushes result.
     public static let OP_ABS: OpCodeProtocol = OpExample() // replaces number with its absolute value
-    public static let OP_NOT: OpCodeProtocol = OpExample() // replaces number with True if it's zero, False otherwise.
+    public static let OP_NOT: OpCodeProtocol = OpNot() // replaces number with True if it's zero, False otherwise.
     public static let OP_0NOTEQUAL: OpCodeProtocol = OpExample() // replaces number with True if it's not zero, False otherwise.
 
     public static let OP_ADD: OpCodeProtocol = OpExample() // (x y -- x+y)
