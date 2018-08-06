58d94343a3be2e25f4b1f96115a9980dcf4e0311
藤田哲史 <tessitetsusi@gmail.com>
2018-08-06 14:56:27 +0900

    :fire: delete unnecessary try

--- BitcoinCashKit/Core/Scripts/OP_CODE/OP_GREATERTHAN.swift
+++ BitcoinCashKit/Core/Scripts/OP_CODE/OP_GREATERTHAN.swift
@@ -27,17 +27,17 @@ import Foundation
 public struct OpGreaterThan: OpCodeProtocol {
     public var value: UInt8 { return 0xa0 }
     public var name: String { return "OP_GREATERTHAN" }
-    
+
     // (x1 x2 -- out)
     public func execute(_ context: ScriptExecutionContext) throws {
         try prepareExecute(context)
         try context.assertStackHeightGreaterThan(2)
-        
+
         let x1 = try context.number(at: -2)
         let x2 = try context.number(at: -1)
-        
+
         context.stack.removeLast()
         context.stack.removeLast()
-        try context.pushToStack(x1 > x2)
+        context.pushToStack(x1 > x2)
     }
 }
