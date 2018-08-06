a01c7277a359b5454495700d4b3fa113410e2957
藤田哲史 <tessitetsusi@gmail.com>
2018-08-06 15:01:55 +0900

    :recycle: fix with SwiftLint

--- BitcoinCashKit/Core/Scripts/OP_CODE/OP_NUMEQUAL.swift
+++ BitcoinCashKit/Core/Scripts/OP_CODE/OP_NUMEQUAL.swift
@@ -27,15 +27,15 @@ import Foundation
 public struct OpNumEqual: OpCodeProtocol {
     public var value: UInt8 { return 0x9c }
     public var name: String { return "OP_NUMEQUAL" }
-    
+
     // (x1 x2 -- out)
     public func execute(_ context: ScriptExecutionContext) throws {
         try prepareExecute(context)
         try context.assertStackHeightGreaterThan(2)
-        
+
         let x1 = try context.number(at: -2)
         let x2 = try context.number(at: -1)
-        
+
         context.stack.removeLast()
         context.stack.removeLast()
         context.pushToStack(x1 == x2)
