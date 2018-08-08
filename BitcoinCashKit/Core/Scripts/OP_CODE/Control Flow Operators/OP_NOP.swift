b2683a18952a455826aa5224c2018605979d5ff7
Shun Usami <usatie@gmail.com>
2018-08-08 23:34:28 +0900

    :sparkles: Implement OP_NOP

--- BitcoinCashKit/Core/Scripts/OP_CODE/Control Flow Operators/OP_NOP.swift
+++ BitcoinCashKit/Core/Scripts/OP_CODE/Control Flow Operators/OP_NOP.swift
@@ -8,10 +8,12 @@
 
 import Foundation
 
+// do nothing
 public struct OpNop: OpCodeProtocol {
     public var value: UInt8 { return 0x61 }
     public var name: String { return "OP_NOP" }
     
     public func mainProcess(_ context: ScriptExecutionContext) throws {
+        // do nothing
     }
 }
