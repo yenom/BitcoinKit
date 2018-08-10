f493974b251fc48e6fa1a03657d3e3c2a3eedd55
藤田哲史 <tessitetsusi@gmail.com>
2018-08-10 16:45:50 +0900

    :recycle: fix name

--- BitcoinCashKit/Core/Scripts/OP_CODE/Stack/OP_TOTALSTACK.swift
+++ BitcoinCashKit/Core/Scripts/OP_CODE/Stack/OP_TOTALSTACK.swift
@@ -25,10 +25,10 @@
 import Foundation
 
 // Puts the input onto the top of the alt stack. Removes it from the main stack.
-public struct OpTotalStack: OpCodeProtocol {
+public struct OpToAltStack: OpCodeProtocol {
     public var value: UInt8 { return 0x6b }
     public var name: String { return "OP_TOALTSTACK" }
-
+    
     // input : x
     // output : (alt)x
     public func mainProcess(_ context: ScriptExecutionContext) throws {
