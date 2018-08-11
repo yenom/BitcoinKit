d23e3a0500a6a699f781f43cc8b2c8d71b26a858
藤田哲史 <tessitetsusi@gmail.com>
2018-08-11 13:11:51 +0900

    :pencil2: add OP_SPLIT

---
+++ BitcoinCashKit/Core/Scripts/OP_CODE/Splice/OP_SPLIT.swift
@@ -0,0 +1,48 @@
+//
+//  OP_SPLIT.swift
+//
+//  Copyright © 2018 BitcoinCashKit developers
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+
+// Split the operand at the given position.
+public struct OpSplit: OpCodeProtocol {
+    public var value: UInt8 { return 0x7f }
+    public var name: String { return "OP_SPLIT" }
+
+    // input : in position
+    // output : x1 x2
+    public func mainProcess(_ context: ScriptExecutionContext) throws {
+        try context.assertStackHeightGreaterThanOrEqual(2)
+        let data = context.data(at: -2)
+        let position = try context.number(at: -1)
+        guard position <= MemoryLayout.size(ofValue: data) else {
+            throw OpCodeExecutionError.error("Invalid OP_SPLIT range")
+        }
+
+        let n1 = data.subdata(in: Range(0..<Int(position)))
+        let n2 = data.subdata(in: Range(Int(position)..<data.count))
+
+        context.stack[data.count - 2] = n1
+        context.stack[data.count - 1] = n2
+    }
+}
