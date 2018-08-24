c039e223fbc2692956c97bdcd6f0bdd9318916dc
藤田哲史 <tessitetsusi@gmail.com>
2018-08-24 14:36:52 +0900

    :recycle: update BU backed seeder

--- Sources/BitcoinKit/Core/Network.swift
+++ Sources/BitcoinKit/Core/Network.swift
@@ -124,7 +124,7 @@ public class BCHMainnet: Mainnet {
         return [
             "seed.bitcoinabc.org", // - Bitcoin ABC seeder
             "seed-abc.bitcoinforks.org", // - bitcoinforks seeders
-            "seed.bitcoinunlimited.info", // - BU seeder
+            "btccash-seeder.bitcoinunlimited.info", // - BU seeder
             "seed.bitprim.org", // - Bitprim
             "seed.deadalnix.me" // - Amaury SÉCHET
         ]
