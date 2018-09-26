ca10bf0513ae5ebda170ea943d0714ffa3e86c1d
藤田哲史 <tessitetsusi@gmail.com>
2018-09-26 20:04:45 +0900

    :recycle: rename tests

--- Tests/BitcoinKitTests/SerializationTests.swift
+++ Tests/BitcoinKitTests/SerializationTests.swift
@@ -36,14 +36,14 @@ class SerializationTests: XCTestCase {
         super.tearDown()
     }
     
-    func test1() {
+    func testUInt32toHex() {
 		let d = Data(bytes: [1, 2, 3, 4])
 		let i: UInt32 = d.to(type: UInt32.self)
 		XCTAssertEqual(i.hex, "04030201")
 		XCTAssertEqual(i, 0x04030201)
 	}
 	
-	func test2() {
+	func testDataToUInt256() {
 		let d = Data(bytes: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2])
 		let i: UInt256 = d.to(type: UInt256.self)
 		XCTAssertEqual(i.hex, "0200000000000000000000000000000000000000000000000000000000000001")
