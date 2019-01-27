cb9ae8bad6a1da7038848892acd2869c3119bb4a
藤田哲史 <akifuji@protonmail.com>
2019-01-27 13:21:27 +0900

    :sparkles: connect to remote node

--- Sources/BitcoinKit/Networking/PeerGroup.swift
+++ Sources/BitcoinKit/Networking/PeerGroup.swift
@@ -25,101 +25,40 @@
 
 import Foundation
 
-public class PeerGroup: PeerDelegate {
-    public let blockChain: BlockChain
-    public let maxConnections: Int
+public class PeerGroup {
+    private let network: Network
+    private let concurrentPeersQueue = DispatchQueue(label: "com.BitcoinKit.peersQueue", attributes: .concurrent)
+    private var unsafePeers = [Peer]()
+    private var peers: [Peer] {
+        var peersCopy: [Peer]!
+        concurrentPeersQueue.sync {
+            peersCopy = self.unsafePeers
+        }
+        return peersCopy
+    }
+    private let maxConnections: UInt
 
-    public weak var delegate: PeerGroupDelegate?
-
-    var peers = [String: Peer]()
-
-    private var filters = [Data]()
-    private var transactions = [Transaction]()
-
-    public init(blockChain: BlockChain, maxConnections: Int = 1) {
-        self.blockChain = blockChain
+    public init(network: Network, maxConnections: UInt) {
+        self.network = network
         self.maxConnections = maxConnections
     }
 
     public func start() {
-        let network = blockChain.network
-        for _ in peers.count..<maxConnections {
-            let peer = Peer(host: network.dnsSeeds[1], network: network)
-            peer.delegate = self
+        for _ in 0..<maxConnections {
+            // TODO: select peer from db
+            let dnsSeeds: [String] = network.dnsSeeds
+            let peer = Peer(host: dnsSeeds[Int(arc4random_uniform(UInt32(dnsSeeds.count)))], network: network)
             peer.connect()
-
-            peers[peer.host] = peer
-        }
-
-        delegate?.peerGroupDidStart(self)
-    }
-
-    public func stop() {
-        for peer in peers.values {
-            peer.delegate = nil
-            peer.disconnect()
-        }
-        peers.removeAll()
-
-        delegate?.peerGroupDidStop(self)
-    }
-
-    // filter: pubkey, pubkeyhash, scripthash, etc...
-    public func addFilter(_ filter: Data) {
-        filters.append(filter)
-    }
-
-    public func sendTransaction(transaction: Transaction) {
-        if let peer = peers.values.first {
-            peer.sendTransaction(transaction: transaction)
-        } else {
-            transactions.append(transaction)
-            start()
+            addPeer(peer)
         }
     }
 
-    public func peerDidConnect(_ peer: Peer) {
-        if peers.filter({ $0.value.context.isSyncing }).isEmpty {
-            let latestBlockHash = blockChain.latestBlockHash()
-            peer.startSync(filters: filters, latestBlockHash: latestBlockHash)
-        }
-        if !transactions.isEmpty {
-            for transaction in transactions {
-                peer.sendTransaction(transaction: transaction)
+    private func addPeer(_ peer: Peer) {
+        concurrentPeersQueue.async(flags: .barrier) { [weak self] in
+            guard let self = self else {
+                return
             }
+            self.unsafePeers.append(peer)
         }
     }
-
-    public func peerDidDisconnect(_ peer: Peer) {
-        peers[peer.host] = nil
-        start()
-    }
-
-    public func peer(_ peer: Peer, didReceiveVersionMessage message: VersionMessage) {
-        if message.userAgent?.value.contains("Bitcoin ABC:0.16") == true {
-            print("it's old version. Let's try to disconnect and connect to aother peer.")
-            peer.disconnect()
-        }
-    }
-
-    public func peer(_ peer: Peer, didReceiveMerkleBlockMessage message: MerkleBlockMessage, hash: Data) {
-        try! blockChain.addMerkleBlock(message, hash: hash)
-    }
-
-    public func peer(_ peer: Peer, didReceiveTransaction transaction: Transaction, hash: Data) {
-        try! blockChain.addTransaction(transaction, hash: hash)
-        delegate?.peerGroupDidReceiveTransaction(self)
-    }
-}
-
-public protocol PeerGroupDelegate: class {
-    func peerGroupDidStart(_ peerGroup: PeerGroup)
-    func peerGroupDidStop(_ peerGroup: PeerGroup)
-    func peerGroupDidReceiveTransaction(_ peerGroup: PeerGroup)
-}
-
-extension PeerGroupDelegate {
-    public func peerGroupDidStart(_ peerGroup: PeerGroup) {}
-    public func peerGroupDidStop(_ peerGroup: PeerGroup) {}
-    public func peerGroupDidReceiveTransaction(_ peerGroup: PeerGroup) {}
 }
