32cd90128546d14ee28a86fe3ea6fe38476f17a2
Shun Usami <usatie@gmail.com>
2019-09-14 23:20:37 +0900

    :sparkles: Add TransactionPlan, TransactionPlanner and tests

---
+++ Sources/BitcoinKit/Wallet/TransactionPlan.swift
@@ -0,0 +1,44 @@
+//
+//  TransactionPlan.swift
+//  
+//  Copyright © 2019 BitcoinKit developers
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+
+/// Describes a preliminary transaction plan.
+public struct TransactionPlan {
+    /// Selected UTXOs.
+    public let utxos: [UnspentTransaction]
+    /// Amount to be received at the other end.
+    public let amount: UInt64
+    /// Estimated transaction fee.
+    public let fee: UInt64
+    /// Change.
+    public let change: UInt64
+
+    public init(utxos: [UnspentTransaction], amount: UInt64, fee: UInt64, change: UInt64) {
+        self.utxos = utxos
+        self.amount = amount
+        self.fee = fee
+        self.change = change
+    }
+}
